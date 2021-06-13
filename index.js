669c921f5a64cc5734f655fdd5313b78e5c83a79
Buse Şentürk <buse.senturk@outlook.com>
2021-06-13 15:29:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 class Breakfast {
   constructor(food, drink) {
-    this.food = egg;
-    this.drink = tea;
+    this.food = food;
+    this.drink = drink;
   }
 }
 
 class Lunch {
   constructor(salad, soup, drink) {
-    this.salad = ceasar;
-    this.soup = tomato;
-    this.drink = water;
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
   }
 }// Write your code here
